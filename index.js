f7f4d9b597fc10191cd0c717dad490cd85ebae43
Jose Barrios <jose@cognilab.com>
2017-06-21 13:18:50 -0700

    Added type checks for properties

--- index.js
+++ index.js
@@ -13,19 +13,51 @@ class PostalAddress extends Multiple(Thing, Intangible, StructuredValue, Contact
     model = model || {};
     super(model);
 
-    this.xxx = model.xxx;
-
+    this.addressCountry = model.addressCountry;
+    this.addressLocality = model.addressLocality;
+    this.addressRegion = model.addressRegion;
+    this.postOfficeBoxNumber = model.postOfficeBoxNumber;
+    this.postalCode = model.postalCode;
+    this.streetAddress = model.streetAddress;
   }
 
-  get xxx(){ return this.computed.xxx; }
-  set xxx(value){
-    if(Thing.isEmpty(value)){ this.computed.xxx = EMPTY}
-    else if(Thing.isString(value)){ this.computed.xxx = value }
-    else if(Thing.isObject(value)){ this.computed.xxx = value }
-    else if(Thing.isArray(value)){ this.computed.xxx = value }
-    else { Thing.logError(this.constructor.name+': xxx must be a string, object, or array', 'type') }
+  get addressCountry(){ return this.computed.addressCountry; }
+  set addressCountry(value){
+    if(Thing.isEmpty(value)){ this.computed.addressCountry = EMPTY}
+    else if(Thing.isString(value)){ this.computed.addressCountry = value }
+    else if(Thing.isObject(value)){ this.computed.addressCountry = value }
+    else { Thing.logError(this.constructor.name+': addressCountry must be a string, or object', 'type') }
   }
-
+  get addressLocality(){ return this.computed.addressLocality; }
+  set addressLocality(value){
+    if(Thing.isEmpty(value)){ this.computed.addressLocality = EMPTY}
+    else if(Thing.isString(value)){ this.computed.addressLocality = value }
+    else { Thing.logError(this.constructor.name+': addressLocality must be a string', 'type') }
+  }
+  get addressRegion(){ return this.computed.addressRegion; }
+  set addressRegion(value){
+    if(Thing.isEmpty(value)){ this.computed.addressRegion = EMPTY}
+    else if(Thing.isString(value)){ this.computed.addressRegion = value }
+    else { Thing.logError(this.constructor.name+': addressRegion must be a string', 'type') }
+  }
+  get postOfficeBoxNumber(){ return this.computed.postOfficeBoxNumber; }
+  set postOfficeBoxNumber(value){
+    if(Thing.isEmpty(value)){ this.computed.postOfficeBoxNumber = EMPTY}
+    else if(Thing.isString(value)){ this.computed.postOfficeBoxNumber = value }
+    else { Thing.logError(this.constructor.name+': postPOfficeBoxNumber must be a string', 'type') }
+  }
+  get postalCode(){ return this.computed.postalCode; }
+  set postalCode(value){
+    if(Thing.isEmpty(value)){ this.computed.postalCode = EMPTY}
+    else if(Thing.isString(value)){ this.computed.postalCode = value }
+    else { Thing.logError(this.constructor.name+': postalCode must be a string', 'type') }
+  }
+  get streetAddress(){ return this.computed.streetAddress; }
+  set streetAddress(value){
+    if(Thing.isEmpty(value)){ this.computed.streetAddress = EMPTY}
+    else if(Thing.isString(value)){ this.computed.streetAddress = value }
+    else { Thing.logError(this.constructor.name+': streetAddresstreetAddress must be a string', 'type') }
+  }
 }
 
 module.exports = PostalAddress;
